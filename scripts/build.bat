96ca6f96f186cf15b2f779dc4ab24288d8da9d20
nono303 <github@nono303.net>
2021-02-21 18:40:10 +0100

    service mode

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,10 +1,10 @@
 net stop geos
 cd /D D:\github
-pip install -e FORK_geos
+REM pip install -e FORK_geos
 REM ou
-REM cd /D D:\github\FORK_geos
-REM python setup.py install
-cd /D D:\geos\config
-call geos -m D:\geos\mapsources -H 127.0.0.1 -P 5000 --display-host maps.nono303.net --display-scheme https
-REM net start geos
+cd /D D:\github\FORK_geos
+python setup.py install
+REM cd /D D:\geos\config
+REM call geos -m D:\geos\mapsources -H 127.0.0.1 -P 5000 --display-host maps.nono303.net --display-scheme https
+net start geos
 REM pause
